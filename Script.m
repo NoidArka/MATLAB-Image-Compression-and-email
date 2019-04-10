67fc59c6319c7af5b7da94fed7b5e0dc78e5b649
NoidArka <iamarkabagchi@gmail.com>
2019-04-10 15:42:38 +0530

    Update Script.m

--- Script.m
+++ Script.m
@@ -2,10 +2,10 @@ clc
 clear all;
 close all;
 
-imgpath = input('Image Path: ');
+imgpath = input('Image Path (Put it in single quotes): ');
 original = imread(imgpath);
 c = input('Percent Compression: ');
 compressed = imresize(imresize(original,(100-c)/100),100/(100-c));
 imwrite(compressed,'Compressed.jpg','quality',100);
-email = input('E-mail: ');
+email = input('E-mail (Put it in single quotes): ');
 sendmail(email,'Compressed', 'Please open the Attachment provided.', 'INSERT OUTPUT FILE (Compressed.jpg) PATH');
